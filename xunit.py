408eeb908c787ae528b150c8e5aa19a886801d94
shindori fumiya <shindori238@gmail.com>
2024-01-07 22:33:10 +0900

    aa

--- xunit.py
+++ xunit.py
@@ -12,24 +12,21 @@ class TestCase:
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert not test.wasRun
-        test.run()
-        assert test.wasRun
+        self.test.run()
+        assert self.test.wasRun
 
     def testSetUp(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert test.wasSetUp
+        self.test.run()
+        assert self.test.wasSetUp
 
 
 class WasRun(TestCase):
-    def __init__(self, name) -> None:
-        self.wasRun = None
-        super().__init__(name)
-
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
 
     def testMethod(self):
