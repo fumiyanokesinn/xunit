121ab243d2ba713358d9d71bfe5a0e6b1f800017
shindori fumiya <shindori238@gmail.com>
2024-01-08 13:49:31 +0900

    21

--- xunit.py
+++ xunit.py
@@ -1,3 +1,14 @@
+class TestResult:
+    def __init__(self) -> None:
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+
+
 class TestCase:
     def __init__(self, name) -> None:
         self.name = name
@@ -5,10 +16,17 @@ class TestCase:
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
+        return result
 
 
 class WasRun(TestCase):
@@ -20,12 +38,31 @@ class WasRun(TestCase):
         self.wasRun = 1
         self.log = self.log + " testMethod"
 
+    def tearDown(self):
+        self.wasRun = 1
+        self.log = self.log + " tearDown"
+
+    def testBrokenMethod(self):
+        raise Exception
+
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
-        self.test = WasRun("testMethod")
-        self.test.run()
-        assert "setUp testMethod" == self.test.log
+        test = WasRun("testMethod")
+        test.run()
+        assert "setUp testMethod tearDown" == test.log
+
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert "1 run, 0 failed" == result.summary()
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert "1 run, 1 failed" == result.summary()
 
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+# TestCaseTest("testFailedResult").run()
